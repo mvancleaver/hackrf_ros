d3864b499f88988e0ad9d30528e20c96e5ba9c63
Matt VanCleave <mvancleaver@gmail.com>
2026-03-29 21:56:41 -0700

    feat(02-03): add pyserial and hackrf_ros_interfaces to package manifests
    - Add pyserial>=3.5 to install_requires in setup.py
    - Add hackrf_ros_interfaces exec_depend to package.xml
    - Add python3-serial exec_depend for Docker/apt dependency resolution

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=['setuptools', 'pyserial>=3.5'],
     zip_safe=True,
     maintainer='root',
     maintainer_email='root@localhost',
